8e032570fa2074cc15afcc940504c37b5536a01e
udelledo <udelledo@gmail.com>
2020-02-03 02:26:11 +0100

    udelledo/kotlin-transmission-client#2 Enable maven install

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+group = "org.udelledo"
+version = "1.0-SNAPSHOT"
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
@@ -10,9 +13,11 @@ compileTestKotlin.kotlinOptions {
 }
 plugins {
     kotlin("jvm") version "1.3.61"
+    maven
     jacoco
 }
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 dependencies {
